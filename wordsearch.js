9ed9d0c24718a4eb2a231253e677ec92734d5e46
vagrant <susu94827@gmail.com>
2022-11-23 22:29:02 +0000

    searching words

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    // first we will search on the original letters nested array
+    // we will search on the transpose(letters) array
+    // const lettersT = transpose(letters)
+    const superArr = [letters, transpose(letters)];
+    for (let arr of superArr) {
+        const horizontalJoin = arr.map(ls => ls.join(''))
+        for (l of horizontalJoin) {
+            if (l.includes(word)) return true
+        }
     }
+    return false;
 }
 
+const transpose = function (matrix) {
+    let newMatrix = [];
+    for (let i = 0; i < matrix[0].length; i ++) {
+      for (let j = 0; j < matrix.length; j ++) {
+        if (j === 0) {
+           newMatrix.push([]);
+        }
+        newMatrix[i].push(matrix[j][i]);
+      }
+    }
+    return newMatrix;
+  }
+
 module.exports = wordSearch
+
+
